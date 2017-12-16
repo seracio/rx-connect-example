b637909e57fe90bb07c8a67d1d92e9c6dd09d20b
nicolasmondon <nicolasmondon@gmail.com>
2017-12-16 14:36:45 +0100

    add share replay

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,7 @@
 // @flow
 import { of } from 'rxjs/observable/of';
+import { map, shareReplay } from 'rxjs/operators';
 
-export const hello$ = of('world');
+// use shareReplay
+// see @ https://stackoverflow.com/questions/46207592/rxjs-5-converting-an-observable-to-a-behaviorsubject
+export const hello$ = of('world').pipe(shareReplay(1));
